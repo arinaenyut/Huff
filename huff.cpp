2638e9a6cb979561027c54bba73b129add715bd2
arinaenyut <61383975+arinaenyut@users.noreply.github.com>
2020-03-07 15:50:20 +0700

    Update huff.cpp
    07

--- huff.cpp
+++ huff.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
-using namespase std;
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+
+class Node{
+	public:
+        	int fq;//частота
+                char sym;//символ
+     		Node *left, *right;  
+    		Node(){
+    			left = NULL;
+    			right = NULL;
+    		} 
+ 		Node(Node *L, Node *R){ 
+ 			left =  L;
+                        right = R;
+                        sym = L->sym + R->sym;
+                }
+};
 
 
 int main (){
+	ifstream file ("text.txt", ios::binary);
+	map <char,int> letters;
+	char s;
+	while (!file.eof()){
+		s = file.get();
+		letters[s]++;}
+		
+	
+    
 
 
-
-
-rerurn 0;}
+return 0;}
